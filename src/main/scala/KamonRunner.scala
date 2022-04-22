9c7e0fff48a2d2a714d7dd705a77a3f564b972e3
avelreserch <pavel.oliynyk@gmail.com>
2022-04-22 16:29:13 +0100

    add kamon/prometheus metrics example [3];

--- src/main/scala/KamonRunner.scala
+++ src/main/scala/KamonRunner.scala
@@ -1,6 +1,7 @@
 import kamon.Kamon
 
 import scala.io.StdIn.readLine
+import scala.util.Random
 
 object KamonRunner extends App {
   println("Kamon testing ...")
@@ -11,7 +12,8 @@ object KamonRunner extends App {
     x <- 0 to 1000
   } yield {
     println(s"Data: $x")
-    Kamon.counter("app.orders.sent").withoutTags().increment()
+
+    Kamon.counter("app.orders.sent").withoutTags().increment( Random.nextInt(9) + 1)
   }
 
   println("Press any key ..")
